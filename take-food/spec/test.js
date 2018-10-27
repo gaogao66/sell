3da9730a36ab6874e159b9a627522696b1e2fa2c
gaogao66 <3163846872@qq.com>
2018-10-27 18:01:46 +0800

    first commit css_demo

--- take-food/spec/test.js
+++
@@ -1,9 +0,0 @@
-describe("",function(){
-  it("",function(){
-    let input=[];
-    let summary=bestCharge(input).trim();
-    let excepted=''.trim();
-    expect(excepted).toEqual(summary);
-  });
-
-});
